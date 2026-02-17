f1438384e734359c6318ebb03be118ef8645a294
spnvisualz <spnvisualz@gmail.com>
2026-02-17 20:21:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,6 +37,17 @@
   });
 })();
 
+/* NAV SCROLL EFFECT */
+const nav = document.querySelector(".nav");
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY > 40){
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
+
 /* ACTIVE LINK AUTO-DETECT */
 const links = document.querySelectorAll(".nav-links a");
 const currentPath = window.location.pathname;
